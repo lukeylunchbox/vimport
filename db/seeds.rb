d470feea16c3da3686aa07e9fc04fd2c24d85589
Luke Colcott <git@github.com:lukeylunchbox/test.git>
2018-05-03 14:37:32 +1000

    Add categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# category = Category.create!([{label: "machinery"}])
+# category = Category.create!([{label: "building materials"}, {label: "consumer electronics"}, {label: "home & garden"}, {label: "apparel"}, {label: "packaging & printing"}, {label: "sports & entertainment"}, {label: "automotive & motorcycles"}])
 
 
 
